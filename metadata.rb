b37647ad122d58bf021561914fb47c2d534541a1
Bastian Schwarz <bastian@codename-php.de>
2020-06-27 12:50:00 +0000

    Updated build files

--- metadata.rb
+++ metadata.rb
@@ -5,13 +5,12 @@ maintainer 'Bastian Schwarz'
 maintainer_email 'bastian@codename-php.de'
 license 'Apache-2.0'
 description 'Cookbook to install apache2'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.0'
-chef_version '>= 13.0' if respond_to?(:chef_version)
-issues_url 'https://github.com/codenamephp/chef.cookbook.apache2/issues' if respond_to?(:issues_url)
-source_url 'https://github.com/codenamephp/chef.cookbook.apache2' if respond_to?(:source_url)
+chef_version '>= 13.0'
+issues_url 'https://github.com/codenamephp/chef.cookbook.apache2/issues'
+source_url 'https://github.com/codenamephp/chef.cookbook.apache2'
 
-supports 'debian', '~> 10.0'
+supports 'debian'
 
 depends 'apt'
 depends 'apache2', '~> 8.0'
